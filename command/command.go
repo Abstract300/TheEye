428561f02287c6a65d4a78e9b98fef63681e1e2c
abstract300 <abstractify300@gmail.com>
2020-08-15 16:23:08 +0530

    remove redundancy.

--- command/command.go
+++ command/command.go
@@ -13,21 +13,21 @@ type Route struct {
 	Cmd    map[string]*Command
 }
 
-// Command is a command to be run and its attribues
+// Command is a command to be run and its attribues.
 type Command struct {
 	Name string
 	Desc string
 	Run  func(ctx *Context)
 }
 
-// Context provides context for a command to execute
+// Context provides context for a command to execute.
 type Context struct {
 	ChannelID string
 	Args      []string
 	Session   *discordgo.Session
 }
 
-// NewRoute generates a Route
+// NewRoute generates a Route.
 func NewRoute(prefix string) *Route {
 	return &Route{
 		Prefix: prefix,
@@ -35,7 +35,7 @@ func NewRoute(prefix string) *Route {
 	}
 }
 
-// NewCommand adds a command to a route
+// NewCommand adds a command to a route.
 func (r *Route) NewCommand(name string, run func(ctx *Context)) {
 	cmd := &Command{
 		Name: name,
@@ -48,22 +48,23 @@ func (r *Route) NewCommand(name string, run func(ctx *Context)) {
 func (r *Route) FindCommand(name string) (*Command, error) {
 	cmd := r.Cmd[name]
 	if cmd == nil {
-		return &Command{}, errors.New("Command not found.")
+		return &Command{}, errors.New("command not found")
 	}
+
 	return cmd, nil
 }
 
 // CommandHandler is injected into the CreateMessage event handler to automatically invoke a command.
 func (r *Route) CommandHandler(msg discordgo.Message, session *discordgo.Session) error {
-	channelID := msg.ChannelID
 	content := msg.Content
+
 	cmdName, args, err := parseMessageContent(content, r.Prefix)
 	if err != nil {
 		return errors.Wrap(err, "Cannot handle command.")
 	}
 
 	ctx := &Context{
-		ChannelID: channelID,
+		ChannelID: msg.ChannelID,
 		Args:      args,
 		Session:   session,
 	}
@@ -78,13 +79,13 @@ func (r *Route) CommandHandler(msg discordgo.Message, session *discordgo.Session
 	return nil
 }
 
-// parseMessageContent parses message for the command name and its args
+// parseMessageContent parses message for the command name and its args.
 func parseMessageContent(msg, prefix string) (string, []string, error) {
 	tokens := strings.Split(msg, " ")
 	namePrefix := tokens[0]
 
 	ok := strings.HasPrefix(namePrefix, prefix)
-	if ok != true {
+	if !ok {
 		return "", []string{}, errors.New("Illegal route")
 	}
 
